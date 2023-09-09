b8d34099992ba3b8aa0bbd635c06fdc16bd4db5e
Lars Tiefland <ltiefland@gmail.com>
2023-09-09 23:40:37 +0200

    erster Versuch Kategorien über API zu lesen

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -9,7 +9,20 @@
         public function index()
         {
             $client = new \GuzzleHttp\Client();
-            $response = $client->get( config( "api.url" ) . "product-categories/", [
+            $response = $client->request( "get", config( "api.url" ) . "product-categories/", [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . config( "api.key" ),
+                    "Content-Type"  => "application/json",
+                    "Accept"        => "application/json",
+                ]
+            ] )->getBody()->getContents();
+            dd( $response );
+        }
+
+        public function show( int $id )
+        {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request( "get", config( "api.url" ) . "product-categories/" . $id, [
                 'headers' => [
                     'Authorization' => 'Bearer ' . config( "api.key" ),
                     "Content-Type"  => "application/json",
