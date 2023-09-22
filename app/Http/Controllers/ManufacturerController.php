7b4434b2765f8f324ca0f9e51f66199f144019f8
Lars Tiefland <ltiefland@gmail.com>
2023-09-22 13:20:56 +0200

    Herstellerliste

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -8,6 +8,28 @@
 
     class ManufacturerController extends Controller
     {
+        public function index()
+        {
+            $client = new Client();
+            try
+            {
+                $response = $client->request( "get", config( "api.url" ) . "manufacturers/", [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . config( "api.key" ),
+                        "Content-Type"  => "application/json",
+                        "Accept"        => "application/json",
+                    ]
+                ] )->getBody()->getContents();
+                $herstellerListe = json_decode( $response )->data;
+                return view( "hersteller_liste",
+                             [
+                                 "herstellerListe" => $herstellerListe
+                             ] );
+            }
+            catch ( GuzzleException )
+            {
+            }
+        }
         public function show( string $manufacturer )
         {
             $client = new Client();
